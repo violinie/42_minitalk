5d6768b9b10d028540efbd4372e5a50924cdda1a
Violinie <hancacopakova@gmail.com>
2025-04-25 00:40:08 +0200

    add send_char func which sends it as bits

--- client.c
+++ client.c
@@ -1,5 +1,20 @@
 #include <signal.h>
 #include <unistd.h>
+#include <sys/types.h>
+
+void send_char(pid_t pid, char c)
+{
+	int i = 7;
+	while (i >= 0)
+	{
+		if ((c >> i) & 1) // shift the bit by i places & print the rightmost
+			kill(pid, SIGUSR1); // send 1
+		else
+			kill(pid, SIGUSR2); // send 0
+		usleep(100);
+		i--;
+	}
+}
 
 int	main(int argc, char **argv)
 {
@@ -13,7 +28,7 @@ int	main(int argc, char **argv)
 		pid = pid * 10 + (argv[1][i] - '0');
 		i++;
 	}
-
+	send_char(pid, 'A');
 	kill(pid, SIGUSR1);
 	return (0);
 }
