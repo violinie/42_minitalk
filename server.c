45946b5e6248146181a8bc38b51b6ab6aa57790f
Violinie <hancacopakova@gmail.com>
2025-04-27 11:46:34 +0200

    add storing buffer for the whole message

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: hanacop <hanacop@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 13:57:51 by hanacop           #+#    #+#             */
-/*   Updated: 2025/04/26 15:49:04 by hanacop          ###   ########.fr       */
+/*   Updated: 2025/04/27 11:45:37 by hanacop          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	handle_signal(int sig, siginfo_t *info, void *context)
 {
 	static unsigned char	current_char = 0;
 	static int				bit_count = 0;
+	static char				buffer[10000];
+	static int				buf_index = 0;
 
 	(void)info;
 	(void)context;
@@ -27,9 +29,13 @@ void	handle_signal(int sig, siginfo_t *info, void *context)
 	bit_count++;
 	if (bit_count == 8)
 	{
-		write(1, &current_char, 1);
+		buffer[buf_index++] = current_char;
 		if (current_char == '\0')
+		{
+			write(1, buffer, buf_index - 1);
 			write(1, "\n", 1);
+			buf_index = 0;
+		}
 		current_char = 0;
 		bit_count = 0;
 	}
